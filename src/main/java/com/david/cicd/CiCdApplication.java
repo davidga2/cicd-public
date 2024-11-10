9bf5a4e96c4c9020c4e32722ee9bbc22b646fdf4
jabez <jabezsamuelzero7@gmail.com>
2024-11-10 22:44:01 +0530

    war to jar

--- src/main/java/com/david/cicd/CiCdApplication.java
+++ src/main/java/com/david/cicd/CiCdApplication.java
@@ -6,16 +6,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CiCdApplication extends SpringBootServletInitializer {
+public class CiCdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		// TODO Auto-generated method stub
-		return builder.sources(CiCdApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		// TODO Auto-generated method stub
+//		return builder.sources(CiCdApplication.class);
+//	}
 
 }
